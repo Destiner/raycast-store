abe5e573e5bae4af5812f28a4938324d6a6b4925
Timur Badretdinov <destinerx@gmail.com>
2022-02-11 12:25:27 +0300

    Add Service

---
+++ src/service/index.ts
@@ -0,0 +1,46 @@
+import extension from './extension.json';
+
+interface Extension {
+  id: string;
+  title: string;
+  description: string;
+  icon: string;
+  category: {
+    title: string;
+    id: string;
+  };
+  commands: {
+    icon?: string;
+    title: string;
+    description: string;
+  }[];
+  preferences: {
+    title: string;
+    description: string;
+  }[];
+  author: {
+    name: string;
+    avatar: string;
+  };
+  contributors: {
+    name: string;
+    avatar: string;
+  }[];
+  installs: number;
+  source: string;
+  media: string[];
+}
+
+class Service {
+  async listExtensions() {
+    return [];
+  }
+
+  async getExtension(author: string, id: string): Promise<Extension> {
+    return extension;
+  }
+}
+
+export default Service;
+
+export type { Extension };
