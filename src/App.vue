6616743c066a78f75ed1956d04ad59ce3ccc87ba
Timur Badretdinov <destinerx@gmail.com>
2022-02-11 08:03:43 +0300

    Fix Vue file formatting

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="app">
     <div>
-      <router-link :to="'/'"> Home </router-link>
-      <router-link :to="'/about'"> About </router-link>
+      <router-link :to="'/'">
+        Home
+      </router-link>
+      <router-link :to="'/about'">
+        About
+      </router-link>
     </div>
     <router-view />
   </div>
@@ -10,8 +14,7 @@
 
 <style>
 body {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica,
-    Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
 }
 
 /* Reset */
