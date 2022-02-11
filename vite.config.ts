2375a5fd155a79ec3ed32cde05e637b3020282c5
Timur Badretdinov <destinerx@gmail.com>
2022-02-11 08:01:05 +0300

    Reindent

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
-	plugins: [vue()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, 'src'),
-		},
-	},
-	server: {
-		port: 8080,
-	},
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    port: 8080,
+  },
 });
