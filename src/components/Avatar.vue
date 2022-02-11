d9137bdddbc4fddb5dca04c1773904696692c698
Timur Badretdinov <destinerx@gmail.com>
2022-02-11 13:26:40 +0300

    Add Avatar component

---
+++ src/components/Avatar.vue
@@ -0,0 +1,18 @@
+<template>
+  <img :src="src">
+</template>
+
+<script setup lang="ts">
+defineProps({
+  src: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+img {
+  border-radius: 50%;
+}
+</style>
