cc81b4b42fae96e50c27930c61f460f4a4207967
Timur Badretdinov <destinerx@gmail.com>
2022-02-12 07:06:09 +0300

    Add LinkExternal component

---
+++ src/components/LinkExternal.vue
@@ -0,0 +1,29 @@
+<template>
+  <a
+    :href="url"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    {{ label }} ↗
+  </a>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  url: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+a {
+  color: var(--text-primary);
+  text-decoration: none;
+}
+</style>
