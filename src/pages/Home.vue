dff6dbefb7c6208dbfdb06902018e87390a6a671
Timur Badretdinov <destinerx@gmail.com>
2022-02-12 07:02:47 +0300

    Clean up Home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,25 +1,3 @@
 <template>
-  <div>
-    <h1>Main page</h1>
-    <div>Latest block on Ethereum: {{ block }}</div>
-  </div>
+  <div />
 </template>
-
-<script setup lang="ts">
-import { ref, onMounted } from 'vue';
-
-const block = ref('0');
-
-async function getLatestBlock(): Promise<string> {
-  const timestamp = Math.floor(Date.now() / 1000);
-  const url = `https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=before&apikey=YourApiKeyToken`;
-  const data = await fetch(url);
-  const json = await data.json();
-  const block = json.result;
-  return block;
-}
-
-onMounted(async () => {
-  block.value = await getLatestBlock();
-});
-</script>
