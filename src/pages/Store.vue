8f758f7390adf5e00ce69906b9928991f86bd3d9
Timur Badretdinov <destinerx@gmail.com>
2022-02-13 06:12:31 +0300

    Basic Store page

--- src/pages/Store.vue
+++ src/pages/Store.vue
@@ -1,3 +1,39 @@
 <template>
-  <div />
+  <div class="page">
+    <div class="links">
+      <RouterLink :to="{ name: 'extension', params: { user: 'thomas', extension: 'spotify-controls' } }">
+        Spotify Controls
+      </RouterLink>
+      <RouterLink :to="{ name: 'extension', params: { user: 'rishabswift', extension: 'word-search' } }">
+        Word Search
+      </RouterLink>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<style scoped>
+.page {
+  display: flex;
+}
+
+.links {
+  display: flex;
+  flex-direction: column;
+  margin-top: 64px;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+a {
+  color: var(--color-text-secondary);
+  text-decoration: none;
+}
+
+a:hover {
+  color: var(--color-text-primary);
+}
+</style>
